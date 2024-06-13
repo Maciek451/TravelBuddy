c0e157fe99a4f4d2eea48c3c8b6fc79acfb307d2
Maciek451 <maciektraczyk123@gmail.com>
2024-06-13 15:39:03 +0100

    test commit

--- ASTravelBuddy/app/src/main/java/uk/ac/aber/dcs/chm9360/travelbuddy/MainActivity.kt
+++ ASTravelBuddy/app/src/main/java/uk/ac/aber/dcs/chm9360/travelbuddy/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TravelBuddyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Maciej",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
