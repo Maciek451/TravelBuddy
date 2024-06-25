7a6e181f7bf045d786b8814137306f62bbaa5224
Maciek451 <maciektraczyk123@gmail.com>
2024-06-25 05:00:51 +0100

    Add vertical menu in profile screen

--- ASTravelBuddy/app/src/main/java/uk/ac/aber/dcs/chm9360/travelbuddy/ui/components/AppBarWithArrowBack.kt
+++ ASTravelBuddy/app/src/main/java/uk/ac/aber/dcs/chm9360/travelbuddy/ui/components/AppBarWithArrowBack.kt
@@ -1,15 +1,20 @@
 package uk.ac.aber.dcs.chm9360.travelbuddy.ui.components
 
-import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import uk.ac.aber.dcs.chm9360.travelbuddy.R
@@ -20,31 +25,37 @@ fun AppBarWithArrowBack(
     navController: NavHostController,
     appBarTitle: Int,
 ) {
-    Row {
-        CenterAlignedTopAppBar(
-            title = {
-                Text(stringResource(id = appBarTitle))
-            },
-            navigationIcon = {
-                IconButton(
-                    onClick = { navController.popBackStack() }
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = stringResource(R.string.arrow_back)
-                    )
-                }
-            },
-            actions = {
-                IconButton(
-                    onClick = { /* Handle overflow menu action */ }
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.MoreVert,
-                        contentDescription = stringResource(R.string.more_icon)
-                    )
-                }
+    var isMenuExpanded by rememberSaveable { mutableStateOf(false) }
+
+    CenterAlignedTopAppBar(
+        title = { Text(stringResource(id = appBarTitle)) },
+        navigationIcon = {
+            IconButton(onClick = { navController.popBackStack() }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = stringResource(R.string.arrow_back))
             }
-        )
-    }
+        },
+        actions = {
+            IconButton(onClick = { isMenuExpanded = !isMenuExpanded }) {
+                Icon(Icons.Filled.MoreVert, contentDescription = stringResource(R.string.more_icon))
+            }
+            DropdownMenu(
+                expanded = isMenuExpanded,
+                onDismissRequest = { isMenuExpanded = false }
+            ) {
+                // Define your menu items here
+                DropdownMenuItem(
+                    text = { Text(stringResource(R.string.delete_acount)) },
+                    onClick = { /* Handle click on menu item 1 */ }
+                )
+                DropdownMenuItem(
+                    text = { Text(stringResource(R.string.remove_all_data)) },
+                    onClick = { /* Handle click on menu item 1 */ }
+                )
+                DropdownMenuItem(
+                    text = { Text(stringResource(R.string.log_out)) },
+                    onClick = { /* Handle click on menu item 1 */ }
+                )
+            }
+        }
+    )
 }
