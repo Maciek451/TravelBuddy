d0542560a563fe7c1fd736edacda4b28ff1185de
Maciek451 <maciektraczyk123@gmail.com>
2024-06-25 04:49:18 +0100

    Add tabs on friends screen

--- ASTravelBuddy/app/src/main/java/uk/ac/aber/dcs/chm9360/travelbuddy/ui/friends/FriendsScreen.kt
+++ ASTravelBuddy/app/src/main/java/uk/ac/aber/dcs/chm9360/travelbuddy/ui/friends/FriendsScreen.kt
@@ -1,16 +1,86 @@
 package uk.ac.aber.dcs.chm9360.travelbuddy.ui.friends
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import uk.ac.aber.dcs.chm9360.travelbuddy.R
 import uk.ac.aber.dcs.chm9360.travelbuddy.ui.components.TopLevelScaffold
+import uk.ac.aber.dcs.chm9360.travelbuddy.ui.theme.backgroundLight
 
 @Composable
-fun FriendsScreen(
-    navController: NavHostController
-) {
+fun FriendsScreen(navController: NavHostController) {
     val appBarTitle = stringResource(R.string.friends)
+    var selectedTabIndex by rememberSaveable { mutableStateOf(0) }
 
-    TopLevelScaffold(navController = navController, appBarTitle = appBarTitle)
+    TopLevelScaffold(
+        navController = navController,
+        appBarTitle = appBarTitle,
+        ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .padding(start = 8.dp, end = 8.dp)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TabRow(
+                    selectedTabIndex = selectedTabIndex
+                ) {
+                    Tab(
+                        text = { Text(stringResource(id = R.string.trips)) },
+                        selected = selectedTabIndex == 0,
+                        onClick = { selectedTabIndex = 0 }
+                    )
+                    Tab(
+                        text = { Text(stringResource(id = R.string.phrases)) },
+                        selected = selectedTabIndex == 1,
+                        onClick = { selectedTabIndex = 1 }
+                    )
+                }
+                when (selectedTabIndex) {
+                    0 -> {
+                        //content goes here
+                    }
+                    1 -> {
+                        //content goes here
+                    }
+                }
+            }
+        }
+    }
 }
