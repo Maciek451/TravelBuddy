34d6e9f12017d1f68403d1a87836d674b078ecd7
Maciek451 <maciektraczyk123@gmail.com>
2024-07-24 23:20:46 +0100

    Add latest minutes and TripDetails screen

---
+++ ASTravelBuddy/app/src/main/java/uk/ac/aber/dcs/chm9360/travelbuddy/ui/my_trips/TripDetails.kt
@@ -0,0 +1,14 @@
+package uk.ac.aber.dcs.chm9360.travelbuddy.ui.my_trips
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import uk.ac.aber.dcs.chm9360.travelbuddy.ui.FirebaseViewModel
+
+@Composable
+fun TripDetailsScreen(
+    navController: NavHostController,
+    firebaseViewModel: FirebaseViewModel = viewModel()
+) {
+
+}
